c68a19664cc77b15a2fd8c42b0e14e34c4830d26
César Mata <cesarmatamoya@gmail.com>
2020-07-16 15:58:20 -0400

    Case zmz

--- zmz.sh
+++ zmz.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-#OPC=`zenity --list --column=OPCION $( echo -e "Lista\nSimple\nLeer\nBorrar" )`
+OPC=`zenity --list --radiolist --column=OPCION --column=ACCION --hide-header $( echo -e "1 Lista\n2 Simple\n3 Leer\n4 Borrar" )`
 
-LIS=`gammu getallmemory ME 2>/dev/null | grep -e 'general' -e 'Nombre' | cut -d ':' -f 2`
-A1=`zenity --list --column=COLUMNA $LIS`
-A2=`zenity --text-info --editable`
-#A=`zenity --forms --add-entry=Texto --text=SMS`
-#A1=`echo $A | cut -d '|' -f 1`
-#A2=`echo $A | cut -d '|' -f 2`
+case $OPC in
+Lista)
 
-zenity --question --default-cancel --text=`echo -e $A2\n$A1`
+	LIS=`gammu getallmemory ME 2>/dev/null | grep -e 'general' -e 'Nombre' | cut -d ':' -f 2`
+	A1=`zenity --list --column=COLUMNA $LIS`
+	A2=`zenity --text-info --editable`
 
-if [ $? -eq 0 ] ;  then  
-	S=`echo $A2 | gammu sendsms TEXT $A1`
-	zenity  --info --text=$S
-else
-	echo "no"
-fi
+	zenity --question --default-cancel --text=`echo -e "$A2\n$A1"`
+
+	if [ $? -eq 0 ] ;  then
+		S=`echo $A2 | gammu sendsms TEXT $A1`
+		zenity  --info --text=$S
+	else
+		echo "no"
+	fi
+;;
+
+Simple)
+	A=`zenity --forms --add-entry=Numero --add-entry=Texto --text=SMS`
+	A1=`echo $A | cut -d '|' -f 1`
+	A2=`echo $A | cut -d '|' -f 2`
+
+	zenity --question --default-cancel --text=$A1
+
+	if [ $? -eq 0 ] ;  then
+		S=`echo $A2 | gammu sendsms TEXT $A1`
+		zenity  --info --text=$S
+	else
+		echo "no"
+	fi
+;;
+
+esac
